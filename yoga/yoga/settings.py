293b7ab7bd4ed7f69e88068070b5b48dbe5c34fc
saisankar <saisankar3193@gmail.com>
2024-03-21 19:58:07 +0530

    update

--- yoga/yoga/settings.py
+++ yoga/yoga/settings.py
@@ -81,6 +81,23 @@ DATABASES = {
     }
 }
 
+# # MY SQL DB
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'klyoga$Feedback',
+#         'USER': 'klyoga',
+#         'PASSWORD': 'Yallaiah@34',
+#         'HOST': 'klyoga.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#
+#     }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
